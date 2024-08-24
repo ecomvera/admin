6d0bea3ed94cd9dd9ef2a26853d69a852bc37df6
sandeep kumar <sandeepypb@gmail.com>
2024-08-24 23:13:01 +0530

    build

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  serverComponentsExternalPackages: ["@aws-sdk/client-s3", "@aws-sdk/s3-request-presigner"],
   images: {
     remotePatterns: [
       {
