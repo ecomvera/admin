0c0440263e2b1790327d38b9ab5d8657094a4b49
sandeep kumar <sandeepypb@gmail.com>
2024-08-26 12:37:53 +0530

    🎨 Style: improve design

--- components/shared/ProductDetails.tsx
+++ components/shared/ProductDetails.tsx
@@ -25,34 +25,29 @@ const LeftGallaryView = ({ images }: { images: IProduct["images"] }) => {
   };
 
   return (
-    <div className="gap-2 tablet:gap-5 flex justify-center py-8 h-fit">
-      <div className="w-[80px]">
-        {images.map((image, index) => (
-          <div key={index} className="flex items-center cursor-pointer border m-1 h-20 overflow-hidden bg-gray-100">
-            <Image
-              src={image.url}
-              alt="product"
-              width={80}
-              height={0}
-              sizes="100vw"
-              onClick={() => handleSlideChange(image.url)}
-              className="object-cover"
-            />
-          </div>
-        ))}
-      </div>
+    <>
+      <div className="flex justify-center h-[330px] mobile:h-[420px]">
+        <div className="relative flex flex-col w-[80px] h-full">
+          {images.map((image, index) => (
+            <div key={index} className="relative cursor-pointer border m-[2px] h-[80px] bg-gray-100">
+              <Image
+                layout="fill"
+                key={index}
+                src={image.url}
+                alt="product"
+                objectFit="contain"
+                objectPosition="center"
+                onClick={() => handleSlideChange(image.url)}
+              />
+            </div>
+          ))}
+        </div>
 
-      <div className="">
-        <Image
-          src={currentSlide}
-          alt="product"
-          width={400}
-          height={0}
-          sizes="100vw"
-          onError={() => setCurrentSlide("/assets/not-found.jpg")}
-        />
+        <div className="relative w-[350px] border ">
+          <Image layout="fill" src={currentSlide} quality={100} objectFit="contain" objectPosition="center" alt="product" />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
@@ -60,39 +55,37 @@ const ProductDetail = ({ data }: { data: IProduct }) => {
   const sizes = data.sizes;
 
   return (
-    <div className="flex-1 px-2 tablet:px-5 laptop:px-10">
-      <h2 className="text-3xl text-dark-3 font-bold mt-8">
-        <Link href={`/e/${data._id}?path=/p/${data._id}`}>
-          <Button variant="outline" size="lg" className="mr-2 text-lg border-blue-700">
-            Edit
-          </Button>
-        </Link>{" "}
-        {data.name}
-      </h2>
-      <p className="mt-2 text-lg">{data.description}</p>
+    <div className="w-full px-2 tablet:px-5 laptop:px-10 mt-5 tablet:mt-0">
+      <Link href={`/e/${data._id}?path=/p/${data._id}`}>
+        <Button variant="outline" size="sm" className="mr-2 text-lg border-blue-700">
+          Edit
+        </Button>
+      </Link>
 
-      <p className="text-3xl font-semibold mt-5">Rs. {data.price}</p>
-      <p className="text-sm font-normal text-light-3">MRP {data.mrp}</p>
-
+      <h2 className="head-text text-dark-3 font-bold mt-5">{data.name}</h2>
+      <p className="text-sm mobile:text-lg">{data.description}</p>
+      <p className="head-text font-semibold mt-5">
+        Rs. {data.price} <span className="text-sm font-normal text-light-3">incl. of all taxes</span>
+      </p>
+      <p className="text-sm font-normal text-light-3 line-through">MRP {data.mrp}</p>
       <div className="border w-fit border-light-3 px-3 my-5">
-        <p className="text-lg font-semibold text-light-3">{data.material}</p>
+        <p className="text-base font-semibold text-light-3">{data.material}</p>
       </div>
-
-      <p className="text-lg font-semibold text-dark-3 uppercase mt-10">Product Sizes</p>
-      <div className="flex gap-2 mt-3">
+      <p className="text-base mobile:text-lg font-semibold text-dark-3 uppercase mt-5">Select Size</p>
+      <div className="flex gap-1">
         {sizes.map((size) => (
           <div key={size} className="border border-light-3 px-3 py-1 cursor-pointer">
-            <p className="text-lg font-semibold text-dark-3">{size}</p>
+            <p className="text-base mobile:text-lg font-semibold text-dark-3">{size}</p>
           </div>
         ))}
       </div>
-
-      <p className="text-2xl font-semibold text-dark-3 mt-10">Key Highlights</p>
-      <div className="pt-5">
-        {data.attributes.map(({ key, value }) => (
-          <div className="flex flex-col" key={key}>
-            <p className="text-lg font-semibold text-dark-3">{key}</p>
-            <p className="text-lg font-normal text-light-3">{value}</p>
+      <p className="text-xl font-semibold text-dark-3 mt-10">Key Highlights</p>
+      <div className="grid grid-cols-2 gap-5 mt-3">
+        {data.attributes.map((item, index) => (
+          <div key={index} className="text-lg text-dark-3">
+            <p className="font-semibold">{item.key}</p>
+            <p>{item.value}</p>
+            <div className="w-full mobile:w-1/2 h-[1px] bg-light-3"></div>
           </div>
         ))}
       </div>
