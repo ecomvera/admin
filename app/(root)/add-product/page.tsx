105d78e4ae9a93b07c52231976a4ca93a220f72d
sandeep kumar <sandeepypb@gmail.com>
2024-09-01 19:35:33 +0530

    🔨 Refactor: prod check

--- app/(root)/add-product/page.tsx
+++ app/(root)/add-product/page.tsx
@@ -1,23 +1,25 @@
 "use client";
 
 import AddProduct from "@/components/forms/AddProduct";
-import { fetcher } from "@/lib/utils";
-import useSWR from "swr";
+// import { fetcher } from "@/lib/utils";
+// import useSWR from "swr";
 
 const Page = () => {
-  const categories = useSWR("/api/categories", fetcher);
-  const attributes = useSWR("/api/attributes", fetcher);
+  // const categories = useSWR("/api/categories", fetcher);
+  // const attributes = useSWR("/api/attributes", fetcher);
 
   return (
     <div>
       <h2 className="head-text py-8">Add Product</h2>
 
-      <AddProduct
-        categories={{ data: categories?.data?.data || [], isLoading: categories.isLoading }}
-        attributesData={{ data: attributes?.data?.data || [], isLoading: attributes.isLoading }}
-      />
+      <AddProduct categories={{ data: [], isLoading: false }} attributesData={{ data: [], isLoading: false }} />
     </div>
   );
 };
 
 export default Page;
+
+// const categories = useSWR("/api/categories", fetcher, {
+//   revalidateOnFocus: false, // Disable revalidation on focus
+//   dedupingInterval: 60000, // Reuse cache for 60 seconds
+// });
--- app/(root)/categories/page.tsx
+++ app/(root)/categories/page.tsx
@@ -3,22 +3,22 @@
 import CategoryTabs from "@/components/shared/CategoryTabs";
 import Attributes from "@/components/shared/Attributes";
 import ListCatgorires from "@/components/shared/ListCatgorires";
-import { fetcher } from "@/lib/utils";
-import useSWR from "swr";
+// import { fetcher } from "@/lib/utils";
+// import useSWR from "swr";
 
 const Page = () => {
-  const categories = useSWR("/api/categories", fetcher);
-  const attributes = useSWR("/api/attributes", fetcher);
-  const parentCategories = useSWR("/api/categories?no-children", fetcher);
+  // const categories = useSWR("/api/categories", fetcher);
+  // const attributes = useSWR("/api/attributes", fetcher);
+  // const parentCategories = useSWR("/api/categories?no-children", fetcher);
 
   return (
     <div className="flex flex-col gap-5 tablet:flex-row">
       <div className="w-full">
         <h2 className="head-text py-8">Add Category</h2>
-        <CategoryTabs parentCategories={parentCategories?.data?.data || []} isLoading={parentCategories.isLoading} />
-        <Attributes attributes={attributes?.data?.data || []} />
+        <CategoryTabs parentCategories={[]} isLoading={false} />
+        <Attributes attributes={[]} />
       </div>
-      <ListCatgorires isLoading={categories.isLoading} allCategories={categories?.data?.data || []} />
+      <ListCatgorires isLoading={false} allCategories={[]} />
     </div>
   );
 };
