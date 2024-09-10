90cc58b87260634685d08cf2e77e9fe250348714
sandeep kumar <sandeepypb@gmail.com>
2024-09-10 16:22:00 +0530

    ✨ Feat: applying where conditions

--- app/api/categories/[slug]/route.ts
+++ app/api/categories/[slug]/route.ts
@@ -1,36 +1,68 @@
-import Category from "@/lib/models/category.model";
-import { connectDB } from "@/lib/mongoose";
+import { prisma } from "@/lib/prisma";
 import type { NextApiRequest } from "next";
 import { NextResponse } from "next/server";
 
-export async function GET(req: NextApiRequest, { params }: { params: { slug: string } }) {
-  await connectDB();
+const convertObject = (obj: Record<string, string>) => {
+  const result: Record<string, string[]> = {};
 
+  Object.keys(obj).forEach((key) => {
+    let value = obj[key].replace("-", " ");
+    result[key] = value.split("_");
+  });
+
+  return result;
+};
+
+export async function GET(req: NextApiRequest, { params }: { params: { slug: string } }) {
   const url = new URL(req.url || "");
   const searchParams = Object.fromEntries(url.searchParams.entries());
 
   const { slug } = params;
+  const output = convertObject(searchParams);
+
+  const conditionsArr: any[] = [];
+
+  // Add filters by priority
+  if (output.color && output.color.length) {
+    conditionsArr.push({ colors: { hasSome: output.color } });
+  }
+
+  if (output.sizes && output.sizes.length) {
+    conditionsArr.push({ sizes: { some: { key: { in: output.sizes } } } });
+  }
+
+  if (output.fit && output.fit.length) {
+    conditionsArr.push({ attributes: { some: { value: { in: output.fit } } } });
+  }
+
+  if (output.sleeve && output.sleeve.length) {
+    conditionsArr.push({ attributes: { some: { value: { in: output.sleeve } } } });
+  }
+
+  if (output.neck && output.neck.length) {
+    conditionsArr.push({ attributes: { some: { value: { in: output.neck } } } });
+  }
 
   try {
     const start = Date.now();
-    const data = await Category.findOne({ slug })
-      .populate({
-        path: "products",
-        model: "Product",
-        select: { category: 0, subCategory: 0 },
-      })
-      .populate({
-        path: "parentId",
-        model: "Category",
-        select: "_id name slug",
-      })
-      .exec();
+    const data = await prisma.category.findUnique({
+      where: { slug: slug },
+      include: {
+        parent: { select: { name: true, slug: true } },
+        products: {
+          where: {
+            AND: conditionsArr,
+          },
+          include: { images: true, sizes: true, attributes: true },
+        },
+      },
+    });
     const duration = Date.now() - start;
     console.log("\x1b[32m%s\x1b[0m", `Categories [id] - Database query time: ${duration} ms`);
 
     return NextResponse.json({
       ok: true,
-      data,
+      category: data,
     });
   } catch (error: any) {
     console.error(error);
