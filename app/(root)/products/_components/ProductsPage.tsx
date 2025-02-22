dbc6352063c0fd4c7797d4dc048b082168acf2c0
sandeep kumar <sandeepypb@gmail.com>
2025-02-22 19:25:22 +0530

    🐛 Fix: data-table

--- app/(root)/products/_components/ProductsPage.tsx
+++ app/(root)/products/_components/ProductsPage.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import DataTable from "./data-table";
-import { columns } from "./columns";
+// import { columns } from "./columns";
 
 const ProductsPage = ({ products, categories }: { products: any[]; categories: any[] }) => {
   return (
     <div>
-      <DataTable isLoading={false} columns={columns} data={products || []} categories={categories || []} />
+      <DataTable isLoading={false} data={products || []} categories={categories || []} />
     </div>
   );
 };
